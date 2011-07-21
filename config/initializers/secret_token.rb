dd7b601a06228bcc4569c361da6d10de71346214
charliechu <charliechu@techbang.com.tw>
2011-07-21 23:14:54 +0800

    First commit and Only Post Model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '231c816c9bb4e31f02b9612457437ae55c50e7da36001af3924f8bc7264cdc055365814e9e31753b08cec109d051d95ea4eec7a37da29a9a2196d7b892caed80'
